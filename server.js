bc5cab7fed918b51ce6fcfb6ad1dea01e659068a
saddi7087 <51917212+saddi7087@users.noreply.github.com>
2021-12-29 17:41:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 80
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World From Amazon ECS\n')
+  response.end('Hello World From Softrams\n')
 })
 
 server.listen(port)
